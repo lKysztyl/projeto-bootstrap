cdd854467d01395f2fe6a8b5880bcb9ec7e8ea89
Patrick Casimiro <patrickcasimiroz@gmail.com>
2023-08-01 17:40:27 -0300

    feat(js): adição de um redimencionador automatico para as imgs dentro do modal.

--- script.js
+++ script.js
@@ -31,14 +31,26 @@ $(document).ready(function() {
             $('.order-lg-1').find('picture:first-child').remove();
         };
 
-        if (window.screen.width < 550) {
-            let source = $('.order-lg-1 > picture').find('source').eq(3);
-            let srcset = source.attr('srcset');
-            console.log(srcset)
-            srcset = srcset.replace('250x200', '550x350');
-            source.attr('srcset', srcset);
+        if (window.screen.width <= 991) {
+            if (window.screen.width <= 400) {
+                let source = $('.order-lg-1 > picture').find('source').eq(3);
+                let srcset = source.attr('srcset');
+                console.log(srcset)
+                srcset = srcset.replace('250x200', '400x200');
+                source.attr('srcset', srcset);
+            } else if (window.screen.width <= 767) {
+                let source = $('.order-lg-1 > picture').find('source').eq(3);
+                let srcset = source.attr('srcset');
+                srcset = srcset.replace('250x200', '950x300');
+                source.attr('srcset', srcset);
+            } else if (window.screen.width <= 991) {
+                let source = $('.order-lg-1 > picture').find('source').eq(2);
+                let srcset = source.attr('srcset');
+                srcset = srcset.replace('350x350', '1050x250');
+                source.attr('srcset', srcset);
+            };
         };
-        
+
     });
 
     $('#form-modal').on('click', function() {
